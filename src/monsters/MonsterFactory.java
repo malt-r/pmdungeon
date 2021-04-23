eb7d6a1eb5ef66376b43ab3d77adc5b8344c1392
deller <dennis.eller@fh-bielefeld.de>
2021-04-23 22:28:55 +0200

    MonsterType enum removes spell errors in the string bases approach

--- src/monsters/MonsterFactory.java
+++ src/monsters/MonsterFactory.java
@@ -3,12 +3,9 @@ package monsters;
 import main.Game;
 
 public class MonsterFactory {
-  public static Monster createMonster(String monsterType, Game game) throws Exception {
-
-    if(monsterType.equals("demon")) return new DemonMonster(game);
-
-    if(monsterType.equals("lizard")) return new LizardMonster(game);
-
-    throw new Exception("Unknown monsterType");
+  public static Monster createMonster(MonsterType monsterType, Game game) throws Exception{
+    if(monsterType == MonsterType.DEMON) return new DemonMonster(game);
+    if(monsterType == MonsterType.LIZARD) return new LizardMonster(game);
+    throw new Exception("MonsterType no supported");
   }
 }
