e2aaf83241272893b1f32687016d30a4ac3e8ae3
deller <dennis.eller@fh-bielefeld.de>
2021-04-24 18:27:27 +0200

    Added doc for MonsterFactory and MonsterType

--- src/monsters/MonsterType.java
+++ src/monsters/MonsterType.java
@@ -1,5 +1,11 @@
 package monsters;
 
+/**
+ * Definition of monster types.
+ * <p>
+ *     Contains all types of monster that can appear in the game.
+ * </p>
+ */
 public enum MonsterType {
   DEMON,
   LIZARD
