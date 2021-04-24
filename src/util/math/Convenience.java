8e3a3440a76c5d9730b68d5d0284fca4cc46147f
malt_r <malte.r@mail.de>
2021-04-24 22:25:09 +0200

    rename normalizeDelta to scaleDelta. Add doc for Vec class

--- src/util/math/Convenience.java
+++ src/util/math/Convenience.java
@@ -5,19 +5,27 @@ import de.fhbielefeld.pmdungeon.vorgaben.tools.Point;
 public class Convenience {
 
     /**
-     * Normalize the difference-vector between two Points on a defined basis.
+     * Scale the difference-vector between two Points.
      *
      * @param p1                 The origin of the vector
      * @param p2                 The tip of the vector
-     * @param normalizationBasis The basis on which the length of the difference-vector should be normalized.
-     * @return A Point, of which the x and y members represent the components of the normalized vector.
+     * @param scale The scale which should be applied to the difference vector.
+     * @return A Vec of the length specified by scale
      */
-    static public Vec normalizeDelta(Vec p1, Vec p2, float normalizationBasis) {
+    static public Vec scaleDelta(Vec p1, Vec p2, float scale) {
         Vec diff = p2.subtract(p1);
-        return diff.normalize(normalizationBasis);
+        return diff.scale(scale);
     }
 
-    static public Vec normalizeDelta(Point p1, Point p2, float normalizationBasis) {
-        return normalizeDelta(new Vec(p1), new Vec(p2), normalizationBasis);
+    /**
+     * Scale the difference-vector between two Points.
+     *
+     * @param p1                 The origin of the vector
+     * @param p2                 The tip of the vector
+     * @param scale The scale which should be applied to the difference vector.
+     * @return A Vec of the length specified by scale
+     */
+    static public Vec scaleDelta(Point p1, Point p2, float scale) {
+        return scaleDelta(new Vec(p1), new Vec(p2), scale);
     }
 }
