0a78281824a7a68403b79359c7d9c0b4dc0f1064
malt_r <malte.r@mail.de>
2021-05-07 22:08:30 +0200

    added documentation

--- src/progress/Level.java
+++ src/progress/Level.java
@@ -1,5 +1,10 @@
 package progress;
 
+/**
+ * Encapsulates basic calculations for progress. Calculates the amount of xp needed
+ * to reach the next level (currently just a first test).
+ * @author malte
+ */
 public class Level {
     private int level;
     private int xp;
@@ -7,14 +12,26 @@ public class Level {
     private float healthScaleConstant = 3.f;
     private float damageScaleConstant = 2.f;
 
+    /**
+     * constructor.
+     */
     public Level() {
         this.level = 1;
     }
 
+    /**
+     * Get current xp.
+     * @return
+     */
     public int getCurrentXP() {
         return xp;
     }
 
+    /**
+     * Add amount of xp to the current xp counter.
+     * @param amount The amount of xp to add
+     * @return True, if a level up occured (temporary solution and this will likely change).
+     */
     // TODO: provide observable pattern for level up
     public boolean increaseXP(int amount) {
         this.xp += amount;
@@ -29,21 +46,39 @@ public class Level {
         }
     }
 
+    /**
+     * Gets the increment of health, which should be applied to the health of the hero.
+     * @return Currently a constant value. Dependent on the implementation of the handling of modifiers of stats, this will change.
+     */
     public float getHealthIncrementForCurrentLevel() {
         // first test
-        return (int)Math.pow(((double)this.level * (double)this.healthScaleConstant), 2);
+        //return (int)Math.pow(((double)this.level * (double)this.healthScaleConstant), 2);
+        return this.healthScaleConstant;
     }
 
+    /**
+     * Gets the increment of damage, which should be applied to the base damage of the hero.
+     * @return Currently a constant value. Dependent on the implementation of the handling of modifiers of stats, this will change.
+     */
     public float getDamageIncrementForCurrentLevel() {
         // first test
-        return (int)Math.pow(((double)this.level * (double)this.damageScaleConstant), 2);
+        //return (int)Math.pow(((double)this.level * (double)this.damageScaleConstant), 2);
+        return this.damageScaleConstant;
     }
 
+    /**
+     * Calculates the amount of xp left to reach the new level.
+     * @return
+     */
     public int getXPForNextLevelLeft() {
         int total = getXPForNextLevelTotal();
         return total - this.xp;
     }
 
+    /**
+     * Calculates the total amount of xp to acquire to reach the new level.
+     * @return
+     */
     public int getXPForNextLevelTotal() {
         // level = sqrt(XP) / constant
         // XP = (level*constant) ^2
@@ -51,6 +86,10 @@ public class Level {
         return (int)Math.pow(((double)this.level * (double)this.xpConstant), 2);
     }
 
+    /**
+     * Gets the current level.
+     * @return The current level.
+     */
     public int getCurrentLevel() {
         return level;
     }
