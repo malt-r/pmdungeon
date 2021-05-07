68ff984763165b894895157a8ce3e4f8d10b1548
deller <dennis.eller@fh-bielefeld.de>
2021-05-07 22:29:01 +0200

    Added Factory and Type enum for traps for easier creation

---
+++ src/traps/TrapFactory.java
@@ -0,0 +1,22 @@
+package traps;
+
+/**
+ * Factory for creation of traps
+ * <p>
+ *     Creates all types of monster that can appear in the game.
+ * </p>
+ */
+public class TrapFactory {
+  /**
+   * Creates a trap of the specific type
+   *
+   * @param   trapType Type of the trap
+   * @throws  Exception   if monsterType is not supported
+   * @return  A trap, of the specified type.
+   */
+  public static Trap createTrap(TrapType trapType) throws Exception{
+    if(trapType == TrapType.HOLE) return new HoleTrap();
+    if(trapType == TrapType.SPIKES) return new SpikesTrap();
+    throw new Exception("TrapType no supported");
+  }
+}
---
+++ src/traps/TrapType.java
@@ -0,0 +1,12 @@
+package traps;
+
+/**
+ * Definition of trap types.
+ * <p>
+ *     Contains all types of traps that can appear in the game.
+ * </p>
+ */
+public enum TrapType {
+  SPIKES,
+  HOLE
+}
