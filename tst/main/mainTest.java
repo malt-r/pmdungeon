2352d3b94ec503ea41f57c0c5cea5984e24a983d
deller <dennis.eller@fh-bielefeld.de>
2021-05-15 14:16:14 +0200

    Added JUnit with dummy test

---
+++ tst/main/mainTest.java
@@ -0,0 +1,13 @@
+package main;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class mainTest {
+  @Test
+  public void testGetTicket() {
+    assertTrue("not implemented",true);
+  }
+}
+
